15848adf3cf8b1b269df8aa4421836465224a701
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2022-09-11 17:17:26 -0400

    Added files via upload

---
+++ website/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, jsonify, request
+from flask_mysqldb import MySQL,MySQLdb
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'super secret key'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
+
