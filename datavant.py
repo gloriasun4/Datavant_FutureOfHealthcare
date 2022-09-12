c12c4c4572047c2387d46098ced74948c1d28c8a
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2022-09-11 23:46:13 -0400

    Updated datavant.py

--- datavant.py
+++ datavant.py
@@ -14,14 +14,14 @@ df = pd.read_csv("drive/MyDrive/CMS_Inpatient.csv")
 print(df.shape)
 df.head()
 
-noneed = ["DESYNPUF_ID", "CLM_ID", "SEGMENT", "CLM_FROM_DT", "CLM_THRU_DT", 
-          "CLM_PMT_AMT", "NCH_PRMRY_PYR_CLM_PD_AMT", "AT_PHYSN_NPI", "OP_PHYSN_NPI", 
-          "OT_PHYSN_NPI", "CLM_ADMSN_DT", "CLM_PASS_THRU_PER_DIEM_AMT", 
-          "NCH_BENE_IP_DDCTBL_AMT", "NCH_BENE_PTA_COINSRNC_LBLTY_AM", 
+noneed = ["DESYNPUF_ID", "CLM_ID", "SEGMENT", "CLM_FROM_DT", "CLM_THRU_DT",
+          "CLM_PMT_AMT", "NCH_PRMRY_PYR_CLM_PD_AMT", "AT_PHYSN_NPI", "OP_PHYSN_NPI",
+          "OT_PHYSN_NPI", "CLM_ADMSN_DT", "CLM_PASS_THRU_PER_DIEM_AMT",
+          "NCH_BENE_IP_DDCTBL_AMT", "NCH_BENE_PTA_COINSRNC_LBLTY_AM",
           "NCH_BENE_BLOOD_DDCTBL_LBLTY_AM", "CLM_UTLZTN_DAY_CNT", "NCH_BENE_DSCHRG_DT"]
 hcpcs = ["HCPCS_CD_" + str(i) for i in range(1, 46)]
 noneed.extend(hcpcs)
-df.drop(noneed, axis = 1, inplace = True)
+df.drop(noneed, axis=1, inplace=True)
 
 dgns = ["ICD9_DGNS_CD_" + str(i) for i in range(1, 11)]
 prcdr = ["ICD9_PRCDR_CD_" + str(i) for i in range(1, 7)]
@@ -49,8 +49,8 @@ print([len(each) for each in states])
 
 dfs = []
 for i in range(len(states)):
-  df1 = df[df.PRVDR_NUM.isin(states[i])]
-  dfs.append(df1)
+    df1 = df[df.PRVDR_NUM.isin(states[i])]
+    dfs.append(df1)
 
 print(len(dfs))
 
@@ -61,28 +61,28 @@ clms = []
 ic_dgns = []
 ic_prcdr = []
 for data in dfs:
-  adm = data.ADMTNG_ICD9_DGNS_CD.value_counts().to_dict()
-  adms.append(adm)
-  clm = data.CLM_DRG_CD.value_counts().to_dict()
-  clms.append(clm)
-  # dgns = ["ICD9_DGNS_CD_" + str(i) for i in range(1, 11)]
-  d = {}
-  for each in dgns:
-    dgns_val = data[each].value_counts().to_dict()
-    newval = Counter(dgns_val)
-    dd = Counter(d)
-    dd.update(newval)
-    d = dict(dd)
-  ic_dgns.append(d)
-  # prcdr = ["ICD9_PRCDR_CD_" + str(i) for i in range(1, 7)]
-  dpr = {}
-  for ea in prcdr:
-    prcdr_val = data[ea].value_counts().to_dict()
-    newval = Counter(prcdr_val)
-    ddpr = Counter(dpr)
-    ddpr.update(newval)
-    dpr = dict(ddpr)
-  ic_prcdr.append(dpr)
+    adm = data.ADMTNG_ICD9_DGNS_CD.value_counts().to_dict()
+    adms.append(adm)
+    clm = data.CLM_DRG_CD.value_counts().to_dict()
+    clms.append(clm)
+    # dgns = ["ICD9_DGNS_CD_" + str(i) for i in range(1, 11)]
+    d = {}
+    for each in dgns:
+        dgns_val = data[each].value_counts().to_dict()
+        newval = Counter(dgns_val)
+        dd = Counter(d)
+        dd.update(newval)
+        d = dict(dd)
+    ic_dgns.append(d)
+    # prcdr = ["ICD9_PRCDR_CD_" + str(i) for i in range(1, 7)]
+    dpr = {}
+    for ea in prcdr:
+        prcdr_val = data[ea].value_counts().to_dict()
+        newval = Counter(prcdr_val)
+        ddpr = Counter(dpr)
+        ddpr.update(newval)
+        dpr = dict(ddpr)
+    ic_prcdr.append(dpr)
 
 df_adm = pd.DataFrame.from_dict(adms)
 df_clm = pd.DataFrame.from_dict(clms)
@@ -109,13 +109,13 @@ options = [df_adm, df_clm, df_dgns, df_prcdr]
 
 raw_options = []
 for dfm in options:
-  raw_options.append(list(dfm.columns))
+    raw_options.append(list(dfm.columns))
 
 fromdata = {}
 for i in range(len(raw_options)):
-  rng.shuffle(raw_options[i])
-  categorize = np.array_split(raw_options[i], 10)
-  fromdata[i] = categorize
+    rng.shuffle(raw_options[i])
+    categorize = np.array_split(raw_options[i], 10)
+    fromdata[i] = categorize
 
 print(len(list(fromdata.values())[0]))
 
@@ -123,40 +123,41 @@ print(len(list(fromdata.values())[0]))
 all_categories_codes = fromdata.get(1)
 cat_grouped = {}
 for i in range(len(all_categories_codes)):
-  cat_codes = all_categories_codes[i]
-  cat_grouped[i] = options[1][cat_codes]
+    cat_codes = all_categories_codes[i]
+    cat_grouped[i] = options[1][cat_codes]
 
 for i in range(len(cat_grouped.keys())):
-  curdf = cat_grouped.get(i)
-  summ = curdf.sum(axis=1).to_frame()
-  summ.columns = ["Total"]
-  curdf = pd.concat([curdf, summ], axis=1)
-  cat_grouped[i] = curdf
+    curdf = cat_grouped.get(i)
+    summ = curdf.sum(axis=1).to_frame()
+    summ.columns = ["Total"]
+    curdf = pd.concat([curdf, summ], axis=1)
+    cat_grouped[i] = curdf
 
 cat_grouped.get(0).head()
 
 mylist = []
 for i in range(len(cat_grouped.keys())):
-  mylist.append(cat_grouped.get(i).Total)
+    mylist.append(cat_grouped.get(i).Total)
 # state_freq = pd.concat(mylist, axis=1)
 mydf = pd.DataFrame(mylist).T
 # mydf.columns = ["cat_" + str(i) for i in range(len(cat_grouped.keys()))]
-mydf.columns = ["Heart Disease", "Cancer", "Chronic Lower Respiratory Diseases", 
-                "Cerebrovascular Disease", "Alzheimer's Disease", "Diabetes", 
-                "Influence and Pnemonia", "Kidney Disease", "Mental Illness", 
+mydf.columns = ["Heart Disease", "Cancer", "Chronic Lower Respiratory Diseases",
+                "Cerebrovascular Disease", "Alzheimer's Disease", "Diabetes",
+                "Influence and Pnemonia", "Kidney Disease", "Mental Illness",
                 "Chronic Liver Disease"]
-mydf["states"] = [ 'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
-           'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
-           'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
-           'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
-           'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
+mydf["states"] = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+                  'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
+                  'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
+                  'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
+                  'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
 mydf
 
-ax = mydf.plot(kind="bar", figsize=(30,8), title="Category Frequencies Across States", x="states")
+ax = mydf.plot(kind="bar", figsize=(30, 8), title="Category Frequencies Across States", x="states")
 ax.set_xlabel("States")
 ax.set_ylabel("Frequency")
 fig = ax.get_figure()
 fig.savefig("categories_states.png")
+
 
 # Functions that you could call to get the datas
 
@@ -165,70 +166,83 @@ fig.savefig("categories_states.png")
 # Compare_cats: The categories you want to compare
 # Returns: A figure with only the states and categories inputted
 def compare_states(states_list, compare_cats):
-  states_list = [state_idx.get(s) for s in states_list]
-  compare_cats = np.append(compare_cats, ["states"])
-  plot = mydf[compare_cats].iloc[states_list].plot(kind="bar", figsize=(30,8), title="Category Frequencies Across Selected States", x="states")
-  plot.set_xlabel("States")
-  plot.set_ylabel("Frequency")
-  fig = plot.get_figure()
-  fig.savefig("compare_states.png")
+    states_list = [state_idx.get(s) for s in states_list]
+    compare_cats = np.append(compare_cats, ["states"])
+    plot = mydf[compare_cats].iloc[states_list].plot(kind="bar", figsize=(30, 8),
+                                                     title="Category Frequencies Across Selected States", x="states")
+    plot.set_xlabel("States")
+    plot.set_ylabel("Frequency")
+    fig = plot.get_figure()
+    fig.savefig("compare_states.png")
 
 
 # Returns, in order of decreasing prevalence, the top k most prevalent categories in given state
 def k_most_prevalent(k, my_state):
-  my_state = state_idx.get(my_state)
-  order = []
-  for mycol in mydf.columns:
-    if mycol == "states":
-      continue
-    order.append((mycol, mydf.loc[my_state, mycol]))
-  order.sort(key = lambda x: x[1], reverse=True)
-  return [c[0] for c in order[:k]]
+    my_state = state_idx.get(my_state)
+    order = []
+    for mycol in mydf.columns:
+        if mycol == "states":
+            continue
+        order.append((mycol, mydf.loc[my_state, mycol]))
+    order.sort(key=lambda x: x[1], reverse=True)
+    return [c[0] for c in order[:k]]
 
 
 # Finds the similarity score between two states, the lower means more similar.
 def sim_score(state1, state2):
-  if isinstance(state1, str):
-    state1 = state_idx.get(state1)
-  if isinstance(state2, str):
-    state2 = state_idx.get(state2)
-  sqdiff = 0
-  for mycol in mydf.columns:
-    if mycol == "states":
-      continue
-    sqdiff += (mydf.loc[state1, mycol] - mydf.loc[state2, mycol])**2
-  return sqdiff
+    if isinstance(state1, str):
+        state1 = state_idx.get(state1)
+    if isinstance(state2, str):
+        state2 = state_idx.get(state2)
+    sqdiff = 0
+    for mycol in mydf.columns:
+        if mycol == "states":
+            continue
+        sqdiff += (mydf.loc[state1, mycol] - mydf.loc[state2, mycol]) ** 2
+    return sqdiff
+
 
 usstates = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
-           'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
-           'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
-           'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
-           'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
-state_idx = {usstates[i]:i for i in range(len(usstates))}
+            'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
+            'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
+            'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
+            'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
+state_idx = {usstates[i]: i for i in range(len(usstates))}
+
+
 # Finds the top k most similar states
 def top_k_similar_states(k, my_state):
-  my_state = state_idx.get(my_state)
-  simscores = []
-  for s in usstates:
-    if state_idx.get(s) == my_state:
-      continue
-    hw = (s, sim_score(my_state, state_idx.get(s)))
-    simscores.append(hw)
-  simscores.sort(key=lambda x:x[1])
-  return [s[0] for s in simscores[:k]]
+    my_state = state_idx.get(my_state)
+    simscores = []
+    for s in usstates:
+        if state_idx.get(s) == my_state:
+            continue
+        hw = (s, sim_score(my_state, state_idx.get(s)))
+        simscores.append(hw)
+    simscores.sort(key=lambda x: x[1])
+    return [s[0] for s in simscores[:k]]
 
 
 # can then recommend to stock up on the appropriate meds/supplies, as applicable
 
 # sample use
 # choices = ["cat_" + str(i) for i in range(len(cat_grouped.keys()))]
-choices = ["Heart Disease", "Cancer", "Chronic Lower Respiratory Diseases", 
-                "Cerebrovascular Disease", "Alzheimer's Disease", "Diabetes", 
-                "Influence and Pnemonia", "Kidney Disease", "Mental Illness", 
-                "Chronic Liver Disease"]
-wanted_fig = compare_states(rng.choice(usstates, size=rng.integers(1, high=50), replace=False), rng.choice(choices, size=10, replace=False))
-print(k_most_prevalent(rng.integers(1, high=11), rng.choice(usstates)))
-print(top_k_similar_states(rng.integers(1, high=50), rng.choice(usstates)))
+choices = ["Heart Disease", "Cancer", "Chronic Lower Respiratory Diseases",
+           "Cerebrovascular Disease", "Alzheimer's Disease", "Diabetes",
+           "Influence and Pnemonia", "Kidney Disease", "Mental Illness",
+           "Chronic Liver Disease"]
+wanted_fig = compare_states(rng.choice(usstates, size=rng.integers(1, high=50), replace=False),
+                            rng.choice(choices, size=10, replace=False))
+chosenk = rng.integers(1, high=11)
+chosenstate = rng.choice(usstates)
+print("The top k = " + str(chosenk) + " most prevalent categories in chosen state = " + str(chosenstate) + " are: ")
+print(k_most_prevalent(chosenk, chosenstate))
+print("")
+chosenk = rng.integers(1, high=50)
+chosenstate = rng.choice(usstates)
+print("The top k = " + str(chosenk) + " most similar states for chosen state = " + str(chosenstate) + " are: ")
+print(top_k_similar_states(chosenk, chosenstate))
+print("")
 
 # !pip -q install flask-ngrok
 
@@ -238,11 +252,11 @@ print(top_k_similar_states(rng.integers(1, high=50), rng.choice(usstates)))
 # from flask import Flask
 # from flask_ngrok import run_with_ngrok
 # app = Flask(__name__)
-# run_with_ngrok(app)   
-  
+# run_with_ngrok(app)
+
 # @app.route("/")
 # def home():
 #     return "<h1>Please help oh god</h1>"
-    
+
 # app.run()
 
